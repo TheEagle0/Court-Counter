b511408c9d7484f7d670d9f91ba2ee4e38ce7995
TheEagle <ahmedkamar10@gmail.com>
2017-10-12 19:40:49 +0200

    init views

--- Android/projects/Store/app/src/main/java/com/example/theeagle/store/MainActivity.java
+++ Android/projects/Store/app/src/main/java/com/example/theeagle/store/MainActivity.java
@@ -2,36 +2,71 @@ package com.example.theeagle.store;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-     int Permission_All=1;
-    String[]Permissions={Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
+    int Permission_All = 1;
+    String[] Permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    EditText email_et, password_et;
+    Button login_btn, register_btn;
+    TextView forget_password_tv;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (!hasPermission(this,Permissions)){
-            ActivityCompat.requestPermissions(this,Permissions,Permission_All);
+        if (!hasPermission(this, Permissions)) {
+            ActivityCompat.requestPermissions(this, Permissions, Permission_All);
+        }
+        initViews();
+    }
+
+    public boolean hasPermission(Context context, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+    public void initViews(){
+        email_et=findViewById(R.id.email_et);
+        password_et=findViewById(R.id.password_et);
+        login_btn=findViewById(R.id.login_btn);
+        register_btn=findViewById(R.id.register_btn);
+        forget_password_tv=findViewById(R.id.forget_password_tv);
+        login_btn.setOnClickListener(this);
+        register_btn.setOnClickListener(this);
+        forget_password_tv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.login_btn:
+
+                break;
+            case R.id.register_btn:
+                Intent intent=new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.forget_password_tv:
+
+                break;
         }
     }
-public boolean hasPermission(Context context,String...permissions){
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M&& context!=null&&permissions!=null){
-        for(String permission :permissions){
-            if(ActivityCompat.checkSelfPermission(context,permission)!= PackageManager.PERMISSION_GRANTED){
-                return false;
-            }
-
-        }
-        }
-      return true;
-}
 }
