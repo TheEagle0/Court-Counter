2f31834498e0dc2ffdc8d15f3b39c54a6515d7d4
TheEagle <ahmedkamar10@gmail.com>
2017-10-17 19:21:50 +0200

    init commit

--- Android/projects/Store/app/src/main/java/com/example/theeagle/store/RegisterActivity.java
+++ Android/projects/Store/app/src/main/java/com/example/theeagle/store/RegisterActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
 import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
-public ImageView imageView;
+    public ImageView imageView;
     private static final int GALLERY_REQUEST = 1;
     private Uri selectedImageUri;
 
@@ -21,34 +21,37 @@ public ImageView imageView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        imageView=findViewById(R.id.iv);
+        imageView = findViewById(R.id.iv);
         imageView.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
-        Intent intent=new Intent(Intent.ACTION_PICK);
+        Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(intent,GALLERY_REQUEST);
+        startActivityForResult(intent, GALLERY_REQUEST);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
-            selectedImageUri= data.getData();
+            selectedImageUri = data.getData();
             readFileFromSelectedURI();
         }
     }
+
     private void readFileFromSelectedURI() {
-        Cursor cursor = getContentResolver().query(selectedImageUri, new String[]{MediaStore.Images.Media.DATA},null,null, null);
+        Cursor cursor = getContentResolver().query(selectedImageUri,
+                new String[]{MediaStore.Images.Media.DATA}, null, null, null);
         if (cursor != null) {
-            cursor.moveToFirst();
-            String imagePath = cursor.getString(0);
-            cursor.close();
-            Bitmap image = BitmapFactory.decodeFile(imagePath);
-            imageView.setImageBitmap(image);
+            if (cursor.moveToFirst()) {
+                String imagePath = cursor.getString(0);
+                cursor.close();
+                Bitmap image = BitmapFactory.decodeFile(imagePath);
+                imageView.setImageBitmap(image);
+            }
         }
     }
 
