5189de554f4eb49d6e35e412b7201ae60a1011c6
TheEagle <ahmedkamar10@gmail.com>
2017-11-13 15:11:27 +0200

    Main activity

--- Android/projects/E-Library/app/src/main/java/com/example/theeagle/e_library/MainActivity.java
+++ Android/projects/E-Library/app/src/main/java/com/example/theeagle/e_library/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.theeagle.e_library;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        CheckFireBase();
+    }
+    private void CheckFireBase(){
+        if (FirebaseAuth.getInstance().getCurrentUser()==null){
+            startActivity(new Intent(this,LoginActivity.class));
+            finish();
+        }
+        else {
+            startActivity(new Intent(this,MyBooksActivity.class));
+            finish();
+        }
     }
 }
